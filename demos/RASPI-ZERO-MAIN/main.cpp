1acefdfba79f3ee209a0e507b90bcaa0eee2c0fb
= <=>
2020-05-25 22:43:28 -0700

    removed all the unnecesarry targets, and made new one where I'll put my personal stuff

---
+++ demos/RASPI-ZERO-MAIN/main.cpp
@@ -0,0 +1,40 @@
+#include "ch.h"
+#include "hal.h"
+
+static WORKING_AREA(waThread1, 128);
+static msg_t Thread1(void *p) {
+  (void)p;
+  chRegSetThreadName("blinker");
+  while (TRUE) {
+    palClearPad(ONBOARD_LED_PORT, ONBOARD_LED_PAD);
+    chThdSleepMilliseconds(100);
+    palSetPad(ONBOARD_LED_PORT, ONBOARD_LED_PAD);
+    chThdSleepMilliseconds(900);
+  }
+  return 0;
+}
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+  halInit();
+  chSysInit();
+
+  /*
+   * Set mode of onboard LED
+   */
+  palSetPadMode(ONBOARD_LED_PORT, ONBOARD_LED_PAD, PAL_MODE_OUTPUT);
+
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  /*
+   * Events servicing loop.
+   */
+  chThdWait(chThdSelf());
+
+  return 0;
+}
